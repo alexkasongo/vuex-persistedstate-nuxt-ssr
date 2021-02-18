22fc1cdccfa1eeff0be2a6c5ab74e4de7c13a341
Alex Kasongo <alex.kasongolenge@yahoo.com>
2021-02-17 19:42:30 -0800

    added deploy settings

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,12 @@ export default {
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [],
-
+  // Deploy
+  target: 'static',
+  // Router
+  router: {
+    base: '//vuex-persistedstate-nuxt-ssr/',
+  },
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [{ src: '~/plugins/persistedState.client.js' }],
 
